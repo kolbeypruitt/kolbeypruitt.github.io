3af23a93ce3455e0d51cafd6ec8b63369edeff03
Kolbey Pruitt <kolbeypruitt@gmail.com>
2019-04-30 01:30:48 -0500

    no loading bar

--- assets/js/pageLoading.js
+++ assets/js/pageLoading.js
@@ -1,40 +1,40 @@
-(function(){
-  function id(v){ return document.getElementById(v); }
-  function loadbar() {
-    var ovrl = id("overlay"),
-        prog = id("progress"),
-        stat = id("progstat"),
-        img = document.images,
-        c = 0,
-        tot = img.length;
-    if(tot == 0) return doneLoading();
+// (function(){
+//   function id(v){ return document.getElementById(v); }
+//   function loadbar() {
+//     var ovrl = id("overlay"),
+//         prog = id("progress"),
+//         stat = id("progstat"),
+//         img = document.images,
+//         c = 0,
+//         tot = img.length;
+//     if(tot == 0) return doneLoading();
 
-    function imgLoaded(){
-      c += 1;
-      var perc = ((100/tot*c) << 0) +"%";
-      prog.style.width = perc;
-      stat.innerHTML = "Loading "+ perc;
-      if(c===tot) return doneLoading();
-    }
-    function doneLoading(){
+//     function imgLoaded(){
+//       c += 1;
+//       var perc = ((100/tot*c) << 0) +"%";
+//       prog.style.width = perc;
+//       stat.innerHTML = "Loading "+ perc;
+//       if(c===tot) return doneLoading();
+//     }
+//     function doneLoading(){
 
-      document.getElementById('bounce').className = "transition animation";
-      document.getElementById('bounce2').className = "transition animation delay";
+//       document.getElementById('bounce').className = "transition animation";
+//       document.getElementById('bounce2').className = "transition animation delay";
       
-      startBrandEffect()
-      startAboutMeScroller()
+//       startBrandEffect()
+//       startAboutMeScroller()
 
-      ovrl.style.opacity = 0;
-      setTimeout(function(){ 
-        ovrl.style.display = "none";
-      }, 1200);
-    }
-    for(var i=0; i<tot; i++) {
-      var tImg     = new Image();
-      tImg.onload  = imgLoaded;
-      tImg.onerror = imgLoaded;
-      tImg.src     = img[i].src;
-    }    
-  }
-  document.addEventListener('DOMContentLoaded', loadbar, false);
-}());
+//       ovrl.style.opacity = 0;
+//       setTimeout(function(){ 
+//         ovrl.style.display = "none";
+//       }, 1200);
+//     }
+//     for(var i=0; i<tot; i++) {
+//       var tImg     = new Image();
+//       tImg.onload  = imgLoaded;
+//       tImg.onerror = imgLoaded;
+//       tImg.src     = img[i].src;
+//     }    
+//   }
+//   document.addEventListener('DOMContentLoaded', loadbar, false);
+// }());
